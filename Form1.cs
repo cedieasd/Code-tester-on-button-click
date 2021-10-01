3b8ac74eb89d94d07803af55964ff92806e9f30a
John Cedric Gamotin <cedieasd@gmail.com>
2021-10-01 23:41:06 +0800

    Add project files.

---
+++ Form1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Web;
+using MySql.Data.MySqlClient;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;   
+using System.Net;
+using System.Net.Mail;
+using System.Windows.Forms;
+
+namespace code_tester_on_button_click
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+           
+        }
+
+        //next method above
+    }
+}
+
